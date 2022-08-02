ce0dbd0c6a7c4389dabc7221f9cdcdac240094aa
imamyy <amytran2608@gmail.com>
2022-08-01 22:17:36 -0400

    test function

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-console.log("stranger things are coming! World!")
-document.querySelector('h1').innerText = 'Welcome to The Upside Down'
-
-const image = document.querySelector('img')
-console.log(image)
-
-const eggos = document.createElement('img')
-document.querySelector('.container').appendChild(eggos)
-eggos.setAttribute('src', 'https://i.ytimg.com/vi/TgSmPqMGk2g/maxresdefault.jpg')
-eggos.style.width = '50%'
-
-// eggos.remove()
-
+console.log("stranger things are coming!");
+document.querySelector("h1").innerText = "Welcome to The Upside Down";
+const image = document.querySelector("img");
+console.log(image);
+image.style.display = "none";
+image.style.display = "block";
+image.style.filter = "grayscale(100%)";
+image.style.width = "95%";
+image.style.filter = "";
+const eggos = document.createElement("img");
+document.querySelector(".container").appendChild(eggos);
+eggos.setAttribute(
+  "src",
+  "https://i.ytimg.com/vi/TgSmPqMGk2g/maxresdefault.jpg"
+);
+eggos.style.width = "50%";
+eggos.remove();
+document.querySelectorAll("li");
